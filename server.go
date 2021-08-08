b4a0a40b38b559fe0219ed8dcc1e46df574b6113
Amel Muminovic <amcenp@gmail.com>
2021-08-08 16:46:03 +0200

    Custom logger middleware added

--- server.go
+++ server.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"os"
+
 	"github.com/Ammce/pilot/controller"
+	"github.com/Ammce/pilot/middleware"
 	"github.com/Ammce/pilot/service"
 	"github.com/gin-gonic/gin"
 )
@@ -11,19 +16,29 @@ var (
 	VideoController controller.VideoController = controller.New(videoService)
 )
 
+func setupLogOutpu() {
+	f, _ := os.Create("logger.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
-	server := gin.Default()
+
+	setupLogOutpu()
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middleware.Logger())
 
 	server.GET("/posts", func(ctx *gin.Context) {
 		ctx.JSON(200, VideoController.FindAll())
 	})
 
-	server.POST("/posts", func(ctx *gin.Context) {
-		ctx.JSON(200, VideoController.Save(ctx))
+	server.GET("/posts/{id}", func(ctx *gin.Context) {
+		fmt.Println(ctx.Query("id"))
+		ctx.JSON(200, VideoController.FindById(ctx))
 	})
 
-	server.GET("/a1", func(ctx *gin.Context) {
-		ctx.JSON(200, VideoController.FindById(ctx))
+	server.POST("/posts", func(ctx *gin.Context) {
+		ctx.JSON(200, VideoController.Save(ctx))
 	})
 
 	server.Run(":8080")
